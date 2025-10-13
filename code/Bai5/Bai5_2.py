1187786a8ff4b8bed4a53c07418f72b3c446c5b1
dtc2903 <duongtancuong292003@gmail.com>
2025-10-14 01:06:30 +0700

    Update Bài 5

---
+++ code/Bai5/Bai5_2.py
@@ -0,0 +1,6 @@
+from Bai5_1 import vocab
+work_dir = 'E:/Documents/NamHoc2025_2026/MayHocUngDung/work/'
+f = open(work_dir + 'vocab_' + str(len(vocab)) + '_.txt', encoding= 'utf-8', mode ='w')
+for word in vocab:
+    f.write(word + '\n')
+f.close()
